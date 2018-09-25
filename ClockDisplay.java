2276a70092ad948af4fda2fbf7ef46ebbd332022
premiumElevator <43154659+premiumElevator@users.noreply.github.com>
2018-09-24 21:00:36 -0400

    implimented 12 hour internal

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -1,14 +1,14 @@
 
 /**
  * The ClockDisplay class implements a digital clock display for a
- * European-style 24 hour clock. The clock shows hours and minutes. The 
- * range of the clock is 00:00 (midnight) to 23:59 (one minute before 
+ * European-style 24 hour clock. The clock shows hours and minutes. The
+ * range of the clock is 00:00 (midnight) to 23:59 (one minute before
  * midnight).
- * 
+ *
  * The clock display receives "ticks" (via the timeTick method) every minute
  * and reacts by incrementing the display. This is done in the usual clock
  * fashion: the hour increments when the minutes roll over to zero.
- * 
+ *
  * @author Michael Kölling and David J. Barnes
  * @version 2011.07.31
  */
@@ -16,40 +16,50 @@ public class ClockDisplay
 {
     private NumberDisplay hours;
     private NumberDisplay minutes;
+    private boolean isPM;
     private String displayString;    // simulates the actual display
-    
+
     /**
-     * Constructor for ClockDisplay objects. This constructor 
+     * Constructor for ClockDisplay objects. This constructor
      * creates a new clock set at 00:00.
      */
     public ClockDisplay()
     {
-        hours = new NumberDisplay(24);
+        hours = new NumberDisplay(12);
         minutes = new NumberDisplay(60);
+        isPM = false;
         updateDisplay();
     }
 
     /**
      * Constructor for ClockDisplay objects. This constructor
-     * creates a new clock set at the time specified by the 
+     * creates a new clock set at the time specified by the
      * parameters.
      */
-    public ClockDisplay(int hour, int minute)
+    public ClockDisplay(int hour, int minute, boolean isPM)
     {
-        hours = new NumberDisplay(24);
+        hours = new NumberDisplay(13);
         minutes = new NumberDisplay(60);
-        setTime(hour, minute);
+        this.isPM = isPM;
+        setTime(hour, minute, isPM);
     }
 
     /**
      * This method should get called once every minute - it makes
      * the clock display go one minute forward.
      */
+
+    public void toggle()
+    {
+      this.isPM = !isPM;
+    }
     public void timeTick()
     {
         minutes.increment();
         if(minutes.getValue() == 0) {  // it just rolled over!
             hours.increment();
+         if(hours.getValue() == 12)
+         toggle();
         }
         updateDisplay();
     }
@@ -58,10 +68,11 @@ public class ClockDisplay
      * Set the time of the display to the specified hour and
      * minute.
      */
-    public void setTime(int hour, int minute)
+    public void setTime(int hour, int minute, boolean isPM)
     {
         hours.setValue(hour);
         minutes.setValue(minute);
+        this.isPM = isPM;
         updateDisplay();
     }
 
@@ -72,13 +83,21 @@ public class ClockDisplay
     {
         return displayString;
     }
-    
+
     /**
      * Update the internal string that represents the display.
      */
     private void updateDisplay()
     {
-        displayString = hours.getDisplayValue() + ":" + 
-                        minutes.getDisplayValue();
+      if(isPM)
+      {
+        displayString = hours.getDisplayValue() + ":" +
+                        minutes.getDisplayValue() + " PM";
+      }
+      else
+      {
+         displayString = hours.getDisplayValue() + ":" +
+                         minutes.getDisplayValue() + " AM";
+      }
     }
 }
