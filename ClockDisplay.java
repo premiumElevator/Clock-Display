7948149ec7c1194428f21832cd60bcadd7ff1aaa
premiumElevator <43154659+premiumElevator@users.noreply.github.com>
2018-09-30 13:57:09 -0400

    adjusted notes

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -1,7 +1,7 @@
 
 /**
  * The ClockDisplay class implements a digital clock display for a
- * European-style 24 hour clock. The clock shows hours and minutes. The
+ * 12 hour clock. The clock shows hours and minutes. The
  * range of the clock is 00:00 (midnight) to 23:59 (one minute before
  * midnight).
  *
@@ -9,8 +9,8 @@
  * and reacts by incrementing the display. This is done in the usual clock
  * fashion: the hour increments when the minutes roll over to zero.
  *
- * @author Michael Kölling and David J. Barnes
- * @version 2011.07.31
+ * @author Peter Basily
+ * @version 2018.09.30
  */
 public class ClockDisplay
 {
